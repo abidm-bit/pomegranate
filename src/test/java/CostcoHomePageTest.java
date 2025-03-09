8bb1d64910cd8c2d54a724a6229e7c0fda06cfca
abidm-bit <abidmres@gmail.com>
2025-03-08 22:46:25 -0500

    3 entries via DataProvider

--- src/test/java/CostcoHomePageTest.java
+++ src/test/java/CostcoHomePageTest.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class CostcoHomePageTest extends Base{
@@ -13,9 +12,14 @@ public class CostcoHomePageTest extends Base{
         chp = PageFactory.initElements(driver,CostcoHomePage.class);
     }
 
+    // 3 entries, so this test will run 3 times
     @DataProvider(name ="product")
     public Object[][] keyword(){
-        return new Object[][]{{"tv","https://www.costco.com/televisions.html"}};
+        return new Object[][]{
+                {"tv","https://www.costco.com/televisions.html"},
+                {"paper","https://www.costco.com/office-paper.html"},
+                {"golf","https://www.costco.com/sports-fitness-golf.html"}
+        };
     }
 
     @Test(dataProvider = "product")
